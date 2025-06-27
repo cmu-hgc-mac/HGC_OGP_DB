266e85fd4d1cd1bf5267bd0d971df400c768ef74
Joy Zhou <yuntongzhou@outlook.com>
2025-06-27 12:34:59 -0400

    Enhance argument help text in main.py for component type specification; add detailed thickness handling in process_survey.py; create new OGP templates for baseplates and modules.

--- rwOGP/main.py
+++ rwOGP/main.py
@@ -53,7 +53,7 @@ if __name__ == "__main__":
     parser.add_argument("--clear", action='store_true', help="Clear the current inventory. Note that these do not delete the OGP output files. They only remove the files from being marked as uploaded in the inventory.")
     parser.add_argument("--updatedb", action='store_true', help="Update the credentials in the configuration file.")
     parser.add_argument("--updatedir", action='store_true', help="Update the directory paths for OGP outputs/processing in the configuration file.")
-    parser.add_argument("--type", type=str, default='', help="Specify the type of component to process and upload. If not specified, all components will be processed.")
+    parser.add_argument("--type", type=str, default='', help="Specify the type of component to process and upload [baseplates/hexaboards/protomodules/modules]. If not specified, all components will be processed.")
     parser.add_argument("--debug", action='store_true', help="Print debug messages.")
     parser.add_argument("--disable", action='store_true', help="Disable the program from uploading.")
 
