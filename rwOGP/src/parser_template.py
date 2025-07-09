08a4483ed10d2ea917a03d370081924db57341af
Joy Zhou <yuntongzhou@outlook.com>
2025-07-09 15:00:36 -0400

    Add component-specific header templates and update DataParser to utilize them based on output directory name

--- rwOGP/src/parser_template.py
+++ rwOGP/src/parser_template.py
@@ -1,5 +1,5 @@
 # This file contains the template for the parser
-header_template = """
+header_template_bp_hxb = """
 {{ ProjectName }}
 LastModified: {{ LastModifiedDate }} {{ LastModifiedTime }}
 Runtime: {{ RunDate }} {{ RunTime }}
@@ -9,11 +9,32 @@ Geometry: {{ Geometry }}
 Density: {{ Density }}
 Sensor size: {{ SensorSize }}
 Flatness: {{ Flatness }}
-Thickness: {{Thickness}}
+Thickness1: {{ Thickness1 }}
+Thickness2: {{ Thickness2 }}
+Thickness3: {{ Thickness3 }}
+Thickness4: {{ Thickness4 }}
+Thickness5: {{ Thickness5 }}
+Thickness6: {{ Thickness6 }}
 Thickness_Offset: {{Thickness_Offset}}
 Position ID: {{ PositionID }}
 TrayNo: {{ TrayNo }}
 Comment: {{ Comment }}
+"""
+
+header_template_pm_module = """
+{{ ProjectName }}
+LastModified: {{ LastModifiedDate }} {{ LastModifiedTime }}
+Runtime: {{ RunDate }} {{ RunTime }}
+Component ID: {{ ComponentID }}
+Operator: {{ Operator }}
+Geometry: {{ Geometry }}
+Density: {{ Density }}
+Sensor size: {{ SensorSize }}
+Flatness: {{ Flatness }}
+Thickness: {{ Thickness }}
+Thickness_Offset: {{Thickness_Offset}}
+Position ID: {{ PositionID }}
+TrayNo: {{ TrayNo }}
 """
 
 data_template = """
